9a01b3db133e8f4b57f6e798ff662139b9e1ca37
Neha Kumari <neha.kumari-ext@jci.com>
2023-11-16 12:03:08 +0530

    nested mat-table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,120 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { ChangeDetectorRef, Component, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'nested-mat-table';
+  @ViewChildren('innerTables') innerTables!: QueryList<MatTable<Address>> ;
+
+  dataSource!: MatTableDataSource<User> ;
+  usersData: User[] = [];
+  columnsToDisplay = ['name', 'email', 'phone'];
+  innerDisplayedColumns = ['street', 'zipCode', 'city'];
+  expandedElement!: User | null ;
+
+  constructor(
+    private cd: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    USERS.forEach(user => {
+      if (user.addresses && Array.isArray(user.addresses) && user.addresses.length) {
+        this.usersData = [...this.usersData, {...user, addresses: new MatTableDataSource(user.addresses)}];
+      } else {
+        this.usersData = [...this.usersData, user];
+      }
+    });
+    this.dataSource = new MatTableDataSource(this.usersData);
+  }
+
+  toggleRow(element: User) {
+    element.addresses && (element.addresses as MatTableDataSource<Address>).data.length ? (this.expandedElement = this.expandedElement === element ? null : element) : null;
+    this.cd.detectChanges();
+  }
+
 }
+
+export interface User {
+  name: string;
+  email: string;
+  phone: string;
+  addresses?: Address[] | MatTableDataSource<Address>;
+}
+
+export interface Address {
+  street: string;
+  zipCode: string;
+  city: string;
+}
+
+export interface UserDataSource {
+  name: string;
+  email: string;
+  phone: string;
+  addresses?: MatTableDataSource<Address>;
+}
+
+const USERS: User[] = [
+  {
+    name: "Mason",
+    email: "mason@test.com",
+    phone: "9864785214",
+    addresses: [
+      {
+        street: "Street 1",
+        zipCode: "78542",
+        city: "Kansas"
+      },
+      {
+        street: "Street 2",
+        zipCode: "78554",
+        city: "Texas"
+      }
+    ]
+  },
+  {
+    name: "Eugene",
+    email: "eugene@test.com",
+    phone: "8786541234",
+    addresses: [
+      {
+        street: "Street 1",
+        zipCode: "78542",
+        city: "Kansas"
+      },
+      {
+        street: "Street 2",
+        zipCode: "78554",
+        city: "Texas"
+      }
+    ]
+  },
+  {
+    name: "Jason",
+    email: "jason@test.com",
+    phone: "7856452187",
+    addresses: [
+      {
+        street: "Street 5",
+        zipCode: "23547",
+        city: "Utah"
+      },
+      {
+        street: "Street 5",
+        zipCode: "23547",
+        city: "Ohio"
+      }
+    ]
+  }
+];
